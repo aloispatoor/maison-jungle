a6f924187662526b66fadb87aedc78657e8b4f06
Alois Patoor <palois89@posteo.net>
2021-12-27 14:36:13 +0100

    Ajout de light et water dans la plantList

--- src/data/plantList.js
+++ src/data/plantList.js
@@ -4,62 +4,80 @@ export const plantList = [
 		category: 'classique',
 		id: '1ed',
         isBestSale: true,
-        isSpecialOffer: true
+        isSpecialOffer: true,
+		light,
+		water
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
         isBestSale: false,
-        isSpecialOffer: false
+        isSpecialOffer: false,
+		light,
+		water
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
         isBestSale: false,
-        isSpecialOffer: false
+        isSpecialOffer: false,
+		light,
+		water
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
 		id: '4kk',
         isBestSale: false,
-        isSpecialOffer: true
+        isSpecialOffer: true,
+		light,
+		water
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
         isBestSale: false,
-        isSpecialOffer: false
+        isSpecialOffer: false,
+		light,
+		water
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
 		id: '6uo',
         isBestSale: false,
-        isSpecialOffer: false
+        isSpecialOffer: false,
+		light,
+		water
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
         isBestSale : true,
-        isSpecialOffer: false
+        isSpecialOffer: false,
+		light,
+		water
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
         isBestSale: false,
-        isSpecialOffer: true
+        isSpecialOffer: true,
+		light,
+		water
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
 		id: '9vn',
         isBestSale: false,
-        isSpecialOffer: true
+        isSpecialOffer: true,
+		light,
+		water
 	}
 ]
